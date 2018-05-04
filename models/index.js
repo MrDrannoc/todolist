66341a038ed1cdd0a7a4c9df212c0d01c873f728
MrDrannoc <poyer.k@gmail.com>
2018-05-04 14:09:38 +0200

    test server.js

--- models/index.js
+++ models/index.js
@@ -8,11 +8,12 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
+// if (config.use_env_variable) {
+//   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// }
+console.log(sequelize)
 
 fs
   .readdirSync(__dirname)
