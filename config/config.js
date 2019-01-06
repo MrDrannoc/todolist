78b7d5aad5f86a29ba2efba5c15942b3fa6c9e1c
MrDrannoc <poyer.k@gmail.com>
2019-01-06 16:09:23 +0100

    change mdp + socketpath

--- config/config.js
+++ config/config.js
@@ -1,36 +1,36 @@
 module.exports = {
   development: {
     username: 'root',
-    password: '',
+    password: 'Mysql@8503',
     database: 'todolist_dev',
     host: '127.0.0.1',
     dialect: 'mysql',
     dialectOptions: {
-      socketPath: '/srv/run/mysqld/mysqld.sock'
+      socketPath: '/srv/run/mysqld/mysqld/mysqld.sock'
     },
     secret: 'secret',
     operatorsAliases: false
   },
   test: {
     username: 'root',
-    password: '',
+    password: 'Mysql@8503',
     database: 'todolist_test',
     host: '127.0.0.1',
     dialect: 'mysql',
     dialectOptions: {
-      socketPath: '/srv/run/mysqld/mysqld.sock'
+      socketPath: '/srv/run/mysqld/mysqld/mysqld.sock'
     },
     secret: 'secret',
     operatorsAliases: false
   },
   production: {
     username: 'root',
-    password: '',
+    password: 'Mysql@8503',
     database: 'todolist_prod',
     host: '127.0.0.1',
     dialect: 'mysql',
     dialectOptions: {
-      socketPath: '/srv/run/mysqld/mysqld.sock'
+      socketPath: '/srv/run/mysqld/mysqld/mysqld.sock'
     },
     secret: 'secret',
     operatorsAliases: false
